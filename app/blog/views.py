5a86d262802e3862ce5cc57f743992b20642a845
mingyu <rlaalsrbgk@gmail.com>
2018-06-01 16:59:12 +0900

    views와 post_list구현
    views는 post_list.html 경로를 나타내주고 프린트

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,15 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def post_list(request):
-    return HttpResponse('Post List입니다.')
+
+    cur_file_path = os.path.abspath(__file__)
+    dir_name = os.path.dirname(cur_file_path)
+    app_dir_name = os.path.dirname(dir_name)
+    second_dir = os.path.join(app_dir_name, 'templates')
+    third_dir = os.path.join(second_dir, 'blog', 'post_list.html')
+    html = open(third_dir, 'rt').read()
+    return HttpResponse(html)
