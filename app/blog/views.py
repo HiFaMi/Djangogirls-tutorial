d6de7dd1646198af422e5e9445d50d41d9f1267d
mingyu <rlaalsrbgk@gmail.com>
2018-06-04 12:46:26 +0900

    HTML파일을 불러올 때 render_to_string 사용
    TEMPLATES_DIR 지정 후, 해당 경로를 settings모듈의 TEMPLATES['DIRS']에 추가
    render_to_string에서 템플릿 파일을 찾기 위해 주어진 경로는 settings모듈의 TEMPLATES['DIRS']에 있는 폴더 목록을 기준으로 검색한다

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,15 +1,19 @@
 import os
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 def post_list(request):
 
-    cur_file_path = os.path.abspath(__file__)
-    dir_name = os.path.dirname(cur_file_path)
-    app_dir_name = os.path.dirname(dir_name)
-    second_dir = os.path.join(app_dir_name, 'templates')
-    third_dir = os.path.join(second_dir, 'blog', 'post_list.html')
-    html = open(third_dir, 'rt').read()
+    # cur_file_path = os.path.abspath(__file__)
+    # dir_name = os.path.dirname(cur_file_path)
+    # app_dir_name = os.path.dirname(dir_name)
+    # second_dir = os.path.join(app_dir_name, 'templates')
+    # third_dir = os.path.join(second_dir, 'blog', 'post_list.html')
+    # html = open(third_dir, 'rt').read()
+
+    # 경로에 해당하는 HTML파일을 문자열로 로드
+    html = render_to_string('blog/post_list.html')
+    # 가져온 문자열 돌려주기
     return HttpResponse(html)
--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # Django가 render, render_to_string등의 함수로
+            # 템플릿을 불러올 때 기준이 되는 폴더
+            TEMPLATES_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
