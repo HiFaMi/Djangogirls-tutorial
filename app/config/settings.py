384b53671001eb2696504f09ab2f313c18e467d5
mingyu <rlaalsrbgk@gmail.com>
2018-06-04 16:25:40 +0900

    app/static폴더를 STATICFILES_DIRS에 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,8 +14,14 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('BASE_DIR: ', BASE_DIR)
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print('TEMPLATES_DIR: ', TEMPLATES_DIR)
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 
 # Quick-start development settings - unsuitable for production
